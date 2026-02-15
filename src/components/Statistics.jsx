b2403a1a3886b4da2b52a21e1a5b18a9ceac4d83
BhattiYash <yashbhattti@gmail.com>
2026-02-15 13:57:08 +0530

    Seperate UI and Data Logic

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,37 +1,40 @@
 import styles from './statistics.module.css'
 export default function Statistics() {
+    const statisticsData = [{
+        id: 1,
+        count: "08+",
+        description: "Years In Business"
+    },
+    {
+        id: 2,
+        count: "120+",
+        description: "Experts Team"
+    },
+    {
+        id: 3,
+        count: "250+",
+        description: "Successful Projects"
+    },
+    {
+        id: 4,
+        count: "25+",
+        description: "Consistent Clients"
+    },
+    ]
     return (
         <>
-        <div className={styles.container}>
+            <div className={styles.container}>
                 <div className={styles.statistics}>
-                <div className={styles.stats}>
-                    <div className={styles.styleContainer}>
-                    <h5>08+</h5>
-                    <p className="">Years In Business</p>
-                    </div>
+                    {statisticsData.map((item) => (
+                        <div className={styles.stats}>
+                            <div className={styles.styleContainer}>
+                                <h5>{item.count}</h5>
+                                <p>{item.description}</p>
+                            </div>
+                            <div className={styles.vline}></div>
+                        </div>
+                    ))}
                 </div>
-                <div className={styles.vline}></div>
-                <div className={styles.stats}>
-                    <div className={styles.styleContainer}>
-                    <h5>120+</h5>
-                    <p>Experts Team</p>
-                    </div>
-                </div>
-                <div className={styles.vline}></div>
-                <div className={styles.stats}>
-                    <div className={styles.styleContainer}>
-                    <h5>250+</h5>
-                    <p>Successful Projects</p>
-                    </div>
-                </div>
-                <div className={styles.vline}></div>
-                <div className={styles.stats}>
-                    <div className={styles.styleContainer}>
-                    <h5>25+</h5>
-                    <p>Consistent Clients</p>
-                </div>
-                </div>
-            </div>
             </div>
         </>
     )
